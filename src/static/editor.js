57a436fb06677d98a94bee5f8178e524f37121a1
Aeek True <aeektrue@gmail.com>
2023-02-25 17:45:22 +0300

    redesign card editor

--- src/static/editor.js
+++ src/static/editor.js
@@ -1,9 +1,19 @@
 let frontField = $('#front')[0]
 let backField = $('#back')[0]
+let frontForm = $('#front-form')[0]
+let backForm = $('#back-form')[0]
 let previewer = $('#preview')[0]
 
-onEditorInput()
-function onEditorInput(){
+window.onload = init
+function init() {
+    frontField.innerText = frontForm.value
+    backField.innerText = backForm.value
+    onEditorInput()
+}
+
+function onEditorInput() {
     console.log('lol:' + backField.value)
-    previewer.src = "/card_preview?" + "front=" + encodeURIComponent(frontField.value) + "&back=" + encodeURIComponent(backField.value)
+    frontForm.value = frontField.innerText
+    backForm.value = backField.innerText
+    previewer.src = "/card_preview?" + "front=" + encodeURIComponent(frontForm.value) + "&back=" + encodeURIComponent(backForm.value)
 }
